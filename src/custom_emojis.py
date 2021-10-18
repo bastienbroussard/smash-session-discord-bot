36382bcf0b36d85810e451a9f32168063e7bf827
Bastien Broussard <bastien.broussard@airudit.com>
2021-10-18 12:31:36 +0200

    # fixed a bug with emojis

--- src/custom_emojis.py
+++ src/custom_emojis.py
@@ -9,5 +9,5 @@ class CustomEmojis:
     def __init__(self, bot: Bot):
         CustomEmojis.switch_emoji = [emoji for emoji in bot.emojis if emoji.name == 'switch'][0]
         CustomEmojis.screen_emoji = [emoji for emoji in bot.emojis if emoji.name == 'screen'][0]
-        CustomEmojis.adapter_emoji = [emoji for emoji in bot.emojis if emoji.name == 'adapter'][0]
+        CustomEmojis.adapter_emoji = [emoji for emoji in bot.emojis if emoji.name == 'gc'][0]
         print(bot.emojis)
